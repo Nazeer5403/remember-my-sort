adc1382f0bf5274e7e8ea3cbe6e6b6aae75f30cc
hxreborn <32096750+hxreborn@users.noreply.github.com>
2025-12-18 17:29:25 +0100

    feat: new app icon by @0xRJDev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "eu.hxreborn.remembermysort"
         minSdk = 30
         targetSdk = 36
-        versionCode = 122
-        versionName = "1.2.2"
+        versionCode = 123
+        versionName = "1.2.3"
     }
 
     signingConfigs {
